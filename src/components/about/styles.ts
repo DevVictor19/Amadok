84c190e12cbf44dfdbf04891072ae953edda749c
Antonio Victor <Antoniovictor12@live.com>
2022-10-25 20:40:25 -0300

    feat: adicionar styles e responsivo

--- src/components/about/styles.ts
+++ src/components/about/styles.ts
@@ -3,13 +3,26 @@ import { MAIN_THEME } from "../../theme/main";
 
 export const Container = styled.main`
   background-color: ${MAIN_THEME.darkBlue};
-  padding: 120px 64px 0;
+  padding: 120px 64px 40px;
+
+  @media screen and (max-width: 768px) {
+    padding: 120px 24px 168px;
+  }
 `;
 
 export const AboutWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media screen and (max-width: 1280px) {
+    flex-direction: column-reverse;
+    gap: 64px;
+  }
+
+  @media screen and (max-width: 481px) {
+    gap: 32px;
+  }
 `;
 
 export const AboutInfo = styled.div`
@@ -40,9 +53,48 @@ export const AboutInfo = styled.div`
   p:last-of-type {
     margin: 16px 0 24px;
   }
+
+  @media screen and (max-width: 481px) {
+    &::after {
+      content: " ";
+      display: block;
+      background: linear-gradient(#d23bff 35.94%, #38fbff 100%);
+      height: 85%;
+      width: 2px;
+      border-radius: 2px;
+
+      position: absolute;
+      left: 24px;
+      bottom: -60%;
+    }
+
+    h1 {
+      font-size: 2rem;
+      line-height: 44px;
+    }
+
+    p:first-of-type {
+      margin-top: 32px;
+      margin-left: 16px;
+    }
+
+    p:last-of-type {
+      margin: 16px 0 32px 16px;
+    }
+  }
 `;
 
 export const AboutImg = styled.div`
   border-radius: 32px;
   overflow: hidden;
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+
+  @media screen and (max-width: 481px) {
+    max-width: 312px;
+    border-radius: 0;
+  }
 `;
