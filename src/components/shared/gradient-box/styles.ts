df101992cb87090155a6b4656878a8ff369574b6
Antonio Victor <Antoniovictor12@live.com>
2022-10-25 10:36:15 -0300

    feat(gradientBox) criar layout basico

---
+++ src/components/shared/gradient-box/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { MAIN_THEME } from "../../../theme/main";
+
+export const Container = styled.section`
+  background: linear-gradient(90deg, #d23bff 35.94%, #38fbff 100%);
+  padding: 1px;
+`;
+
+export const Content = styled.div`
+  background-color: ${MAIN_THEME.grayBlue};
+  padding: 31px;
+`;
