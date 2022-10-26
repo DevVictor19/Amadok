cd0e25670d84c4128cf767cee37c4404b985709e
Antonio Victor <Antoniovictor12@live.com>
2022-10-26 15:11:42 -0300

    feat(schedule) adicionar componente schedule

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import { Header } from "./components/header";
 import { About } from "./components/about";
+import { Schedule } from "./components/schedule";
 
 export function App() {
   return (
     <React.Fragment>
       <Header />
       <About />
+      <Schedule />
     </React.Fragment>
   );
 }
--- src/theme/main.ts
+++ src/theme/main.ts
@@ -2,5 +2,6 @@ export const MAIN_THEME = {
   darkBlue: "#0F0722",
   grayBlue: "#1C162C",
   pink100: "#EFEBFF",
+  pink500: "#B730FD",
   purpleShade: "rgba(38, 11, 77, 0.1)",
 };
